8d8efeee9f8d692bafed5808cbf0158473fadedd
Nicolas <90246306+nicolascrausaz@users.noreply.github.com>
2021-11-24 15:50:49 +0100

    Implements both methods

--- hash-file/src/app/services/file-hash.service.ts
+++ hash-file/src/app/services/file-hash.service.ts
@@ -8,53 +8,92 @@ export class FileHashService {
 
   constructor() { }
 
+  readonly FILESIZE: number = 100000000; // 1GB
+  readonly CHUNKSIZE: number = Math.pow(2, 27);
+
   /**
    * This function hash a file with sha256
+   * Using Web Crypto API for file < FILESIZE
+   * Using ASMCrypto.js for file >= FILESIZE
    * @param file 
    */
   hash(file: File, filePerso: FileHash) {
+    if (file) file.size < this.FILESIZE ? this.hashSmallFile(file, filePerso) : this.hashBigFile(file, filePerso);
+  }
 
+
+  hashSmallFile(file: File, filePerso: FileHash) {
     // Start counter
     var startTime = performance.now()
 
-    let hash = "Calculating...";
-    let hasher = new Crypto.SHA256();
-    if (file) {
-      let size = file.size;
-      let chunk_size = Math.pow(2, 27);  //Best size found
-      let offset = 0;
+    let chunk_size = this.FILESIZE;
+    let offset = 0;
 
-      let reader = new FileReader();
+    let reader = new FileReader();
 
-      reader.onload = (e) => {
-        if (reader.readyState == FileReader.DONE) {
-          // Hash with sha256 from asmCrypto
-          let uint8_data = new Uint8Array(<ArrayBuffer>e.target.result);
-          hasher.process(uint8_data);  //Hash the partial file
+    reader.onload = async (e) => {
+      if (reader.readyState == FileReader.DONE) {
+        let uint8_data = new Uint8Array(<ArrayBuffer>e.target.result);
 
-          if (offset < size) {
-            // Update offset for next slice
-            offset += chunk_size;
-            reader.readAsArrayBuffer(file.slice(offset, offset + chunk_size));
-          } else {
-            // If we are done, finalize the hash with all hash
-            hasher.finish();
-            hash = this.bytes_to_hex(hasher.result)
-            
-            // Stop counter
-            var endTime = performance.now()
-            console.log(`Hash the file took ${endTime - startTime} milliseconds`)
+        const hashCrypto = await crypto.subtle.digest('SHA-256', uint8_data); // hash file in one time with Javascript Crypto Web API
+        const hashArray = Array.from(new Uint8Array(hashCrypto));  // convert buffer in octet array
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join(''); // convert byte to hex
 
-            filePerso.hash = hash;
-            return;
-          }
-        }
-      };
-      // First call we get 0, chunck_size
-      reader.readAsArrayBuffer(file.slice(offset, offset + chunk_size));
-    }
+        filePerso.hash = hashHex;
+
+        // Stop counter
+        var endTime = performance.now()
+        console.log(`Hash the file took ${endTime - startTime} milliseconds (small file)`)
+
+        return;
+
+      }
+    };
+    // First call we get 0, chunck_size
+    reader.readAsArrayBuffer(file.slice(offset, offset + chunk_size));
+
+
   }
 
+  hashBigFile(file: File, filePerso: FileHash) {
+    // Start counter
+    var startTime = performance.now()
+
+    let hasher = new Crypto.SHA256();
+    let size = file.size;
+    let chunk_size = this.CHUNKSIZE
+    let offset = 0;
+
+    let reader = new FileReader();
+
+    reader.onload = (e) => {
+      if (reader.readyState == FileReader.DONE) {
+        // Hash with sha256 from asmCrypto
+        let uint8_data = new Uint8Array(<ArrayBuffer>e.target.result);
+        hasher.process(uint8_data);  //Hash the partial file
+
+        if (offset < size) {
+          // Update offset for next slice
+          offset += chunk_size;
+          reader.readAsArrayBuffer(file.slice(offset, offset + chunk_size));
+        } else {
+          // If we are done, finalize the hash with all hash
+          hasher.finish();
+          let hash = this.bytes_to_hex(hasher.result)
+
+          // Stop counter
+          var endTime = performance.now()
+          console.log(`Hash the file took ${endTime - startTime} milliseconds (large file)`)
+
+          filePerso.hash = hash;
+          return;
+        }
+      }
+    };
+    // First call we get 0, chunck_size
+    reader.readAsArrayBuffer(file.slice(offset, offset + chunk_size));
+
+  }
 
   // Private methode to convert ann Uint8Array of byte to a string of hexadecimal values
   private bytes_to_hex(arr: Uint8Array) {
